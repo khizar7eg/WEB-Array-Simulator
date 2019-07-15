d135f001be0fd4cdbdf965cdf2ccf00e57de0e42
Khizar Hayat <khizar.hayat7eg@gmail.com>
2019-07-15 20:47:59 +0500

    Array Simulator

---
+++ WEScript.js
@@ -0,0 +1,335 @@
+
+        var array = ["Ball",1,3,"Cat",9,7,5,"Apple"];
+		var itemValue;
+		var indexValue;
+		var value;
+		document.getElementById("output").innerHTML = array;
+
+
+
+
+		function enableFunction1(){
+			document.getElementById("c1").checked = true;
+			document.getElementById("b1").disabled = false;
+			document.getElementById("index").disabled =true;
+			document.getElementById("item").disabled =false;
+
+
+			document.getElementById("c2").checked = false;
+			document.getElementById("c3").checked = false;
+			document.getElementById("c4").checked = false;
+			document.getElementById("c5").checked = false;
+			document.getElementById("c6").checked = false;
+			document.getElementById("c7").checked = false;
+			document.getElementById("c8").checked = false;
+			document.getElementById("c9").checked = false;
+			document.getElementById("c10").checked = false;
+
+			document.getElementById("b2").disabled = true;
+			document.getElementById("b3").disabled = true;
+			document.getElementById("b4").disabled = true;
+			document.getElementById("b5").disabled = true;
+			document.getElementById("b6").disabled = true;
+			document.getElementById("b7").disabled = true;
+			document.getElementById("b8").disabled = true;
+			document.getElementById("b9").disabled = true;
+			document.getElementById("b10").disabled = true;
+
+		}
+		function enableFunction2(){
+			document.getElementById("c2").checked = true;
+			document.getElementById("b2").disabled = false;
+			document.getElementById("index").disabled =true;
+			document.getElementById("item").disabled =true;
+
+			document.getElementById("c1").checked = false;
+			document.getElementById("c3").checked = false;
+			document.getElementById("c4").checked = false;
+			document.getElementById("c5").checked = false;
+			document.getElementById("c6").checked = false;
+			document.getElementById("c7").checked = false;
+			document.getElementById("c8").checked = false;
+			document.getElementById("c9").checked = false;
+			document.getElementById("c10").checked = false;
+
+			document.getElementById("b1").disabled = true;
+			document.getElementById("b3").disabled = true;
+			document.getElementById("b4").disabled = true;
+			document.getElementById("b5").disabled = true;
+			document.getElementById("b6").disabled = true;
+			document.getElementById("b7").disabled = true;
+			document.getElementById("b8").disabled = true;
+			document.getElementById("b9").disabled = true;
+			document.getElementById("b10").disabled = true;
+		}
+		function enableFunction3(){
+			document.getElementById("c3").checked = true;
+			document.getElementById("b3").disabled = false;
+			document.getElementById("index").disabled =true;
+			document.getElementById("item").disabled =true;
+
+			document.getElementById("c2").checked = false;
+			document.getElementById("c1").checked = false;
+			document.getElementById("c4").checked = false;
+			document.getElementById("c5").checked = false;
+			document.getElementById("c6").checked = false;
+			document.getElementById("c7").checked = false;
+			document.getElementById("c8").checked = false;
+			document.getElementById("c9").checked = false;
+			document.getElementById("c10").checked = false;
+
+			document.getElementById("b2").disabled = true;
+			document.getElementById("b1").disabled = true;
+			document.getElementById("b4").disabled = true;
+			document.getElementById("b5").disabled = true;
+			document.getElementById("b6").disabled = true;
+			document.getElementById("b7").disabled = true;
+			document.getElementById("b8").disabled = true;
+			document.getElementById("b9").disabled = true;
+			document.getElementById("b10").disabled = true;
+		}
+		function enableFunction4(){
+			document.getElementById("c4").checked = true;
+			document.getElementById("b4").disabled = false;
+			document.getElementById("index").disabled =true;
+			document.getElementById("item").disabled =true;
+
+			document.getElementById("c2").checked = false;
+			document.getElementById("c3").checked = false;
+			document.getElementById("c1").checked = false;
+			document.getElementById("c5").checked = false;
+			document.getElementById("c6").checked = false;
+			document.getElementById("c7").checked = false;
+			document.getElementById("c8").checked = false;
+			document.getElementById("c9").checked = false;
+			document.getElementById("c10").checked = false;
+
+			document.getElementById("b2").disabled = true;
+			document.getElementById("b3").disabled = true;
+			document.getElementById("b1").disabled = true;
+			document.getElementById("b5").disabled = true;
+			document.getElementById("b6").disabled = true;
+			document.getElementById("b7").disabled = true;
+			document.getElementById("b8").disabled = true;
+			document.getElementById("b9").disabled = true;
+			document.getElementById("b10").disabled = true;
+		}
+		function enableFunction5(){
+			document.getElementById("c5").checked = true;
+			document.getElementById("b5").disabled = false;
+			document.getElementById("item").disabled =true;
+			document.getElementById("index").disabled =false;
+
+			document.getElementById("c2").checked = false;
+			document.getElementById("c3").checked = false;
+			document.getElementById("c4").checked = false;
+			document.getElementById("c1").checked = false;
+			document.getElementById("c6").checked = false;
+			document.getElementById("c7").checked = false;
+			document.getElementById("c8").checked = false;
+			document.getElementById("c9").checked = false;
+			document.getElementById("c10").checked = false;
+
+			document.getElementById("b2").disabled = true;
+			document.getElementById("b3").disabled = true;
+			document.getElementById("b4").disabled = true;
+			document.getElementById("b1").disabled = true;
+			document.getElementById("b6").disabled = true;
+			document.getElementById("b7").disabled = true;
+			document.getElementById("b8").disabled = true;
+			document.getElementById("b9").disabled = true;
+			document.getElementById("b10").disabled = true;
+		}
+		function enableFunction6(){
+			document.getElementById("c6").checked = true;
+			document.getElementById("b6").disabled = false;
+			document.getElementById("index").disabled =true;
+			document.getElementById("item").disabled =true;
+
+			document.getElementById("c2").checked = false;
+			document.getElementById("c3").checked = false;
+			document.getElementById("c4").checked = false;
+			document.getElementById("c5").checked = false;
+			document.getElementById("c1").checked = false;
+			document.getElementById("c7").checked = false;
+			document.getElementById("c8").checked = false;
+			document.getElementById("c9").checked = false;
+			document.getElementById("c10").checked = false;
+
+			document.getElementById("b2").disabled = true;
+			document.getElementById("b3").disabled = true;
+			document.getElementById("b4").disabled = true;
+			document.getElementById("b5").disabled = true;
+			document.getElementById("b1").disabled = true;
+			document.getElementById("b7").disabled = true;
+			document.getElementById("b8").disabled = true;
+			document.getElementById("b9").disabled = true;
+			document.getElementById("b10").disabled = true;
+		}
+		function enableFunction7(){
+			document.getElementById("c7").checked = true;
+			document.getElementById("b7").disabled = false;
+			document.getElementById("index").disabled =true;
+			document.getElementById("item").disabled =false;
+
+			document.getElementById("c2").checked = false;
+			document.getElementById("c3").checked = false;
+			document.getElementById("c4").checked = false;
+			document.getElementById("c5").checked = false;
+			document.getElementById("c6").checked = false;
+			document.getElementById("c1").checked = false;
+			document.getElementById("c8").checked = false;
+			document.getElementById("c9").checked = false;
+			document.getElementById("c10").checked = false;
+
+			document.getElementById("b2").disabled = true;
+			document.getElementById("b3").disabled = true;
+			document.getElementById("b4").disabled = true;
+			document.getElementById("b5").disabled = true;
+			document.getElementById("b6").disabled = true;
+			document.getElementById("b1").disabled = true;
+			document.getElementById("b8").disabled = true;
+			document.getElementById("b9").disabled = true;
+			document.getElementById("b10").disabled = true;
+		}
+		function enableFunction8(){
+			document.getElementById("c8").checked = true;
+			document.getElementById("b8").disabled = false;
+			document.getElementById("item").disabled =true;
+			document.getElementById("index").disabled =false;
+
+			document.getElementById("c2").checked = false;
+			document.getElementById("c3").checked = false;
+			document.getElementById("c4").checked = false;
+			document.getElementById("c5").checked = false;
+			document.getElementById("c6").checked = false;
+			document.getElementById("c7").checked = false;
+			document.getElementById("c1").checked = false;
+			document.getElementById("c9").checked = false;
+			document.getElementById("c10").checked = false;
+
+			document.getElementById("b2").disabled = true;
+			document.getElementById("b3").disabled = true;
+			document.getElementById("b4").disabled = true;
+			document.getElementById("b5").disabled = true;
+			document.getElementById("b6").disabled = true;
+			document.getElementById("b7").disabled = true;
+			document.getElementById("b1").disabled = true;
+			document.getElementById("b9").disabled = true;
+			document.getElementById("b10").disabled = true;
+		}
+		function enableFunction9(){
+			document.getElementById("c9").checked = true;
+			document.getElementById("b9").disabled = false;
+			document.getElementById("item").disabled =true;
+			document.getElementById("index").disabled =true;
+
+			document.getElementById("c2").checked = false;
+			document.getElementById("c3").checked = false;
+			document.getElementById("c4").checked = false;
+			document.getElementById("c5").checked = false;
+			document.getElementById("c6").checked = false;
+			document.getElementById("c7").checked = false;
+			document.getElementById("c8").checked = false;
+			document.getElementById("c1").checked = false;
+			document.getElementById("c10").checked = false;
+
+			document.getElementById("b2").disabled = true;
+			document.getElementById("b3").disabled = true;
+			document.getElementById("b4").disabled = true;
+			document.getElementById("b5").disabled = true;
+			document.getElementById("b6").disabled = true;
+			document.getElementById("b7").disabled = true;
+			document.getElementById("b8").disabled = true;
+			document.getElementById("b1").disabled = true;
+			document.getElementById("b10").disabled = true;
+		}
+		function enableFunction10(){
+			document.getElementById("c10").checked = true;
+			document.getElementById("b10").disabled = false;
+			document.getElementById("item").disabled =true;
+			document.getElementById("index").disabled =true;
+
+			document.getElementById("c2").checked = false;
+			document.getElementById("c3").checked = false;
+			document.getElementById("c4").checked = false;
+			document.getElementById("c5").checked = false;
+			document.getElementById("c6").checked = false;
+			document.getElementById("c7").checked = false;
+			document.getElementById("c8").checked = false;
+			document.getElementById("c9").checked = false;
+			document.getElementById("c1").checked = false;
+
+			document.getElementById("b2").disabled = true;
+			document.getElementById("b3").disabled = true;
+			document.getElementById("b4").disabled = true;
+			document.getElementById("b5").disabled = true;
+			document.getElementById("b6").disabled = true;
+			document.getElementById("b7").disabled = true;
+			document.getElementById("b8").disabled = true;
+			document.getElementById("b9").disabled = true;
+			document.getElementById("b1").disabled = true;
+		}
+
+
+		function pushFunction(){
+			itemValue = document.getElementById("item").value;
+			array.push(itemValue);
+			document.getElementById("output").innerHTML = "Array:" + array;
+		}
+
+		function popFunction(){
+			itemValue = document.getElementById("item").value;
+			array.pop();
+			document.getElementById("output").innerHTML = "Array:" + array;
+		}
+
+		function shiftFunction(){
+			itemValue = document.getElementById("item").value;
+			array.shift();
+			document.getElementById("output").innerHTML = "Array:" + array;
+		}
+
+		function unshiftFunction(){
+			itemValue = document.getElementById("item").value;
+			array.unshift();
+			document.getElementById("output").innerHTML = "Array:" + array;
+		}
+
+		function deleteFunction(){
+			indexValue = document.getElementById("index").value;
+			delete array[indexValue];
+			document.getElementById("output").innerHTML = "Array:" + array;
+		}
+
+		function sortFunction(){
+			array.sort();
+			document.getElementById("output").innerHTML = "Array:" + array;
+		}
+
+		function indexofFunction(){
+			itemValue = document.getElementById("item").value;
+			value = array.indexOf(itemValue);
+			document.getElementById("output").innerHTML = "Array:" + array;
+			document.getElementById("output").innerHTML = "("+ itemValue + ") Index: " + value;
+		}	
+
+		function valueatFunction(){
+			indexValue = document.getElementById("index").value;
+			value = array[indexValue];
+			document.getElementById("output").innerHTML = "Value:" + value;
+		}
+
+		function reverseFunction(){
+			array.reverse();
+			document.getElementById("output").innerHTML = "Array:" + array;
+		}	
+
+		function updateFunction(){
+			array = array;
+			document.getElementById("output").innerHTML = "Array:" + array;
+			document.getElementById("index").value = "";
+			document.getElementById("item").value = "";
+		}
+
+
